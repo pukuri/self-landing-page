53341c12c63193a6db71973f8f26f08f1a6cf513
Fachri Akbar Rahmanda <fachriakbarrahmanda@gmail.com>
2025-12-07 16:10:29 +0700

    change color scheme

--- src/App.tsx
+++ src/App.tsx
@@ -2,19 +2,19 @@ import { Instagram, Linkedin, Github } from "lucide-react";
 
 function App() {
   return (
-    <div className="min-h-screen bg-indigo-900 flex flex-col justify-center items-center text-white px-4">
+    <div className="min-h-screen flex flex-col justify-center items-center px-4">
       <div className="max-w-md text-center">
         <h1 className="text-5xl font-bold">Fachri Akbar Rahmanda</h1>
         <p className="mt-2 text-lg">Software Engineer</p>
-        <div className="flex justify-center mt-4 space-x-4">
+        <div className="flex justify-evenly mt-4 space-x-4">
           <a href="https://www.instagram.com/fachriakbb/" target="_blank" rel="noopener noreferrer">
-            <Instagram className="w-6 h-6" />
+            <Instagram className="w-6 h-6" color="white" />
           </a>
           <a href="https://www.linkedin.com/in/fachriakbr" target="_blank" rel="noopener noreferrer">
-            <Linkedin className="w-6 h-6" />
+            <Linkedin className="w-6 h-6" color="white" />
           </a>
           <a href="https://www.github.com/pukuri" target="_blank" rel="noopener noreferrer">
-            <Github className="w-6 h-6" />
+            <Github className="w-6 h-6" color="white" />
           </a>
         </div>
       </div>
